171ebd0d46a02cf687b6a4b72d4da6a3f554d890
root <edwin.cubillos@uptc.edu.co>
2018-01-04 02:14:39 -0500

    change to 2.0

--- psql_users/psql_users/wsgi.py
+++ psql_users/psql_users/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for users_psql project.
+WSGI config for psql_users project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "users_psql.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psql_users.settings")
 
 application = get_wsgi_application()
