65fa20ec8b3af9884f92b17c806da6621b0aa6ab
EDWIN CUBILLOS <edwin.cubillos@uptc.edu.co>
2018-01-04 08:26:05 -0500

    Custom serializers
    add field to serializers

--- psql_users/api/serializers.py
+++ psql_users/api/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import  serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-
+    email = serializers.SerializerMethodField()
+    
+    def get_email(self, obj):
+        return obj.user.email
+        
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('user', 'email', 'created_at', 'last_login')
