b320725ce5c4ab2b3d32a804861d9fb7d10be11d
root <edwin.cubillos@uptc.edu.co>
2018-01-05 20:26:04 -0500

    fix username serielers

--- psql_users/api/serializers.py
+++ psql_users/api/serializers.py
@@ -3,10 +3,14 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
     
     def get_email(self, obj):
         return obj.user.email
+
+    def get_username(self, obj):
+        return obj.user.username
         
     class Meta:
         model = User
-        fields = ('user', 'email', 'created_at', 'last_login')
+        fields = ('username', 'email', 'created_at', 'last_login')
